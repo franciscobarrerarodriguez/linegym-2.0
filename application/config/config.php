c8db2eb883b5b0f3b5af096e281bbcdf20f1630c
Francisco Javier Barrera Rodriguez <francisco.barrera@uptc.edu.co>
2016-11-27 13:28:07 -0500

    Make it better 2.0

--- application/config/config.php
+++ application/config/config.php
@@ -26,11 +26,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 /* No olvidar configurar para subir al host
 */
 //DEFAULT
-$config['base_url'] = '';
+// $config['base_url'] = '';
 // LOCALHOST
 // $config['base_url'] = 'http://localhost/linegym-2.0/';
 // HOSTING
 // $config['base_url'] = 'http://linegym.virtualtic.co/';
+ $config['base_url'] = 'linegym.herokuapp.com';
 /*
 |--------------------------------------------------------------------------
 | Index File
@@ -376,7 +377,7 @@ $config['encryption_key'] = '';
 $config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = sys_get_temp_dir();
+$config['sess_save_path'] = NULL;
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
