906be9140d7def32a95fad441ef6a1d5a51e6eb1
Francisco Javier Barrera Rodriguez <francisco.barrera@uptc.edu.co>
2016-11-27 12:38:52 -0500

    Make it better 2.0

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'linegym.virtualtic.co',
+	'hostname' => '67.225.203.140',
 	'username' => 'virtualt_root',
 	'password' => '123456789',
 	'database' => 'virtualt_linegymDB',
